1c5cadf7c62b56f332ef6ee7f7186e6a90e03583
Matt Brandt <matt@secretmustache.com>
2025-02-22 18:20:48 -0700

    prometheus - fix django metrics

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('metrics/', include('django_prometheus.urls')),
+    path('', include('django_prometheus.urls')),
     path('', include('blog.urls')),
 ]
