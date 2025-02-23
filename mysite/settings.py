1abc5b23fd90b60bf2b7cfb99e33e2a487bda5b5
Matt Brandt <matt@secretmustache.com>
2025-02-22 19:12:54 -0700

    Move Django debug to env var. Set prom app name

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False')
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
@@ -139,4 +138,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Prometheus
-PROMETHEUS_METRIC_NAMESPACE = "hello_django"
+PROMETHEUS_APP_NAME = "mysite"
+PROMETHEUS_METRIC_LABELS = ["django"]
